21a5acade8fe7fee4355230fdc3ef4c4f1d52af7
Gerrit Schrader <gerrit.schrader@gmail.com>
2018-08-04 21:05:39 +0200

    externalized config with path+method lists

--- src/main/java/demo/PathWatchService.java
+++ src/main/java/demo/PathWatchService.java
@@ -8,40 +8,52 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import demo.config.WatchDirConfig;
 import lombok.extern.apachecommons.CommonsLog;
 
 @Service
 @CommonsLog
+@ConfigurationProperties("watch")
 public class PathWatchService {
 
-  @Value("${watch.path}")
-  String watchPath;
+  @NotEmpty
+  List<WatchDirConfig> directories;
+
+	public void setDirectories(List<WatchDirConfig> directories) {
+		this.directories = directories;
+	}
 
   @Async
   public void start() {
     try {
       WatchService watchService = FileSystems.getDefault().newWatchService();
-      Path path = Paths.get(this.watchPath);
-      path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY,
+      for (WatchDirConfig dir : directories) {
+        Path path = Paths.get(dir.getPath());
+        path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_MODIFY,
           StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.OVERFLOW);
+      }
       WatchKey key;
       while ((key = watchService.take()) != null) {
         for (WatchEvent<?> event : key.pollEvents()) {
-          Path _path = ((WatchEvent<Path>) event).context();
+          Path dir = (Path) key.watchable();
+          Path fullPath = dir.resolve((Path) event.context());
           WatchEvent.Kind kind = event.kind();
           if (kind == StandardWatchEventKinds.ENTRY_CREATE) {
-            log.info("ENTRY_CREATE: " + _path.toString());
+            log.info("CREATE: " + fullPath.getFileName());
           } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
-            log.info("ENTRY_MODIFY: " + _path.toString());
+            log.info("MODIFY: " + fullPath.getFileName());
           } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
-            log.info("ENTRY_DELETE: " + _path.toString());
+            log.info("DELETE: " + fullPath.getFileName());
           } else if (kind == StandardWatchEventKinds.OVERFLOW) {
-            log.info("OVERFLOW: " + _path.toString());
+            log.info("OVERFLOW: " + fullPath.getFileName());
           }
         }
         key.reset();
---
+++ src/main/java/demo/config/WatchDirConfig.java
@@ -0,0 +1,28 @@
+package demo.config;
+
+import javax.validation.constraints.NotBlank;
+
+public class WatchDirConfig {
+
+  @NotBlank
+  String path;
+
+  @NotBlank
+  String format;
+
+  public String getPath() {
+		return this.path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getFormat() {
+		return this.format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+}
